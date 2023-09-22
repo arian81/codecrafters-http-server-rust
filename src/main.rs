0325465d4de7b3eaa8a7ad33a76957e91b7c5d67
Arian Ahmadinejad <35879206+arian81@users.noreply.github.com>
2023-09-22 15:15:13 -0400

    pass stage 4

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io::prelude::*;
 use std::net::TcpListener;
 
@@ -5,17 +6,50 @@ const BUFFER_SIZE: usize = 1024;
 const SUCCESS_HTTP: &str = "HTTP/1.1 200 OK\r\n\r\n";
 const NOTFOUND_HTTP: &str = "HTTP/1.1 404 Not Found\r\n\r\n";
 
+#[derive(Debug, Default)]
+struct HTTTPRequest<'a> {
+    method: &'a str,
+    path: &'a str,
+    headers: HashMap<&'a str, &'a str>,
+    body: String,
+}
+
 fn process(request: [u8; BUFFER_SIZE]) -> Vec<u8> {
-    let reqstr = String::from_utf8(request.to_vec()).unwrap();
-    let mut splitreq: Vec<&str> = reqstr.split("\r\n").collect();
-    splitreq.truncate(splitreq.len() - 2);
-    let req: Vec<&str> = splitreq[0].split_whitespace().collect();
-    let _req_type = req[0];
-    let path = req[1];
+    let mut processed_request = HTTTPRequest::default();
+    let rawreq_str: String = String::from_utf8(request.to_vec()).unwrap();
+    let rawreq_arr: Vec<&str> = rawreq_str.split("\r\n").collect();
+
+    // .unwrap()
+    // .split("\r\n")
+    // .collect();
+    for i in rawreq_arr {
+        if i.contains("GET") || i.contains("POST") {
+            let req: Vec<&str> = i.split_whitespace().collect();
+            processed_request.method = req[0];
+            processed_request.path = req[1];
+        } else if i.contains(": ") {
+            let header: Vec<&str> = i.split(": ").collect();
+            processed_request.headers.insert(header[0], header[1]);
+        } else if !i.is_empty() {
+            let body = i.trim_matches('\0');
+            processed_request.body.push_str(body);
+            processed_request.body.push_str("\n");
+        }
+    }
+
     let mut resp: Vec<u8> = Vec::new();
 
-    if path.eq("/") {
+    if processed_request.path.eq("/") {
         resp.extend(SUCCESS_HTTP.as_bytes());
+    } else if processed_request.path.contains("echo") {
+        resp.extend(
+            format!(
+                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                processed_request.body.len(),
+                processed_request.body
+            )
+            .as_bytes(),
+        );
     } else {
         resp.extend(NOTFOUND_HTTP.as_bytes());
     }
