ece64c609599253d87cb0fe055de50b004ede994
Arian Ahmadinejad <35879206+arian81@users.noreply.github.com>
2023-09-22 16:20:06 -0400

    done stage 6

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
+use std::env::{self};
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
-use std::thread;
+use std::path::Path;
+use std::{fs, thread};
 
 const BUFFER_SIZE: usize = 1024;
 const SUCCESS_HTTP: &str = "HTTP/1.1 200 OK\r\n\r\n";
@@ -20,9 +22,6 @@ fn process(request: [u8; BUFFER_SIZE]) -> Vec<u8> {
     let rawreq_str: String = String::from_utf8(request.to_vec()).unwrap();
     let rawreq_arr: Vec<&str> = rawreq_str.split("\r\n").collect();
 
-    // .unwrap()
-    // .split("\r\n")
-    // .collect();
     for i in rawreq_arr {
         if i.contains("GET") || i.contains("POST") {
             let req: Vec<&str> = i.split_whitespace().collect();
@@ -42,7 +41,7 @@ fn process(request: [u8; BUFFER_SIZE]) -> Vec<u8> {
 
     if processed_request.path.eq("/") {
         resp.extend(SUCCESS_HTTP.as_bytes());
-    } else if processed_request.path.contains("echo") {
+    } else if processed_request.path.starts_with("/echo") {
         let value = processed_request.path.strip_prefix(r"/echo/").unwrap();
         resp.extend(
             format!(
@@ -62,6 +61,32 @@ fn process(request: [u8; BUFFER_SIZE]) -> Vec<u8> {
             )
             .as_bytes(),
         );
+    } else if processed_request.path.starts_with("/files") {
+        let mut directory: String = String::new();
+        let filename = processed_request.path.strip_prefix("/files/").unwrap();
+        for arg in env::args() {
+            if arg.starts_with("--directory") {
+                directory = arg.trim_start_matches("--directory=").to_string();
+                break;
+            }
+        }
+        match fs::read_to_string(Path::new(&directory).join(filename)) {
+            Ok(contents) => {
+                // Construct the HTTP response
+                let response = format!(
+                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                    contents.len(),
+                    contents
+                );
+
+                // Extend the response Vec<u8>
+                resp.extend(response.as_bytes());
+            }
+            Err(_) => {
+                // Handle the error case
+                resp.extend(b"HTTP/1.1 404 Not Found\r\n\r\nFile not found");
+            }
+        }
     } else {
         resp.extend(NOTFOUND_HTTP.as_bytes());
     }
@@ -77,6 +102,8 @@ fn handle_request(mut stream: TcpStream) {
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
+
+    // let args: Vec<String> = env::args().collect();
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
