31bb13ec44b7ba73b9c0786651548c3f92fe55b6
Arian Ahmadinejad <35879206+arian81@users.noreply.github.com>
2023-09-22 11:59:31 -0400

    pass 1st stage

--- your_server.sh
+++ your_server.sh
@@ -5,8 +5,15 @@
 # CodeCrafters uses this file to test your code. Don't make any changes here!
 #
 # DON'T EDIT THIS!
+# exec cargo run \
+#     --quiet \
+#     --release \
+#     --target-dir=/tmp/codecrafters-http-server-target \
+#     --manifest-path $(dirname $0)/Cargo.toml -- "$@"
+
+
 exec cargo run \
-    --quiet \
     --release \
     --target-dir=/tmp/codecrafters-http-server-target \
     --manifest-path $(dirname $0)/Cargo.toml -- "$@"
+
